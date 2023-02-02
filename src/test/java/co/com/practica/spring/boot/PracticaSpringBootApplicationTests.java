0943b29e52ff21798f6368b744ff3bc76568884f
Dante <jhonalexanderfonseca1993@gmail.com>
2023-02-02 11:01:49 -0500

    Commit inicial

---
+++ src/test/java/co/com/practica/spring/boot/PracticaSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.practica.spring.boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticaSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
