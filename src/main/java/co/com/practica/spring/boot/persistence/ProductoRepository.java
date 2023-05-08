8da1ea1185a78bfa7315c6597b5236c23db0ab10
Alexander Fonseca <jafonsec@bancolombia.com.co>
2023-05-08 16:07:13 -0500

    Se agregan las librerias necesarias para que el proyecto funcione

--- src/main/java/co/com/practica/spring/boot/persistence/ProductoRepository.java
+++ src/main/java/co/com/practica/spring/boot/persistence/ProductoRepository.java
@@ -2,15 +2,38 @@ package co.com.practica.spring.boot.persistence;
 
 import co.com.practica.spring.boot.persistence.crud.IProductoCrudRepository;
 import co.com.practica.spring.boot.persistence.entity.Producto;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
+@Repository
 public class ProductoRepository {
 
     private IProductoCrudRepository iProductoCrudRepository;
 
     public List<Producto> getAll() {
         return (List<Producto>) iProductoCrudRepository.findAll();
+    }
+
+    public List<Producto> getByCategoria(int idCategoria) {
+        return (List<Producto>) iProductoCrudRepository.findByIdCategoriaOrederByNombreAsc(idCategoria);
+    }
+
+    public Optional<List<Producto>> getEscasos(int cantidadStock, boolean estado){
+        return iProductoCrudRepository.findByCantidadStockLessThanAndEstado(cantidadStock,estado);
+    }
+
+    public Optional<Producto> getProducto(int idProducto){
+        return iProductoCrudRepository.findById(idProducto);
+    }
+
+    public Producto save(Producto producto){
+        return iProductoCrudRepository.save(producto);
+    }
+
+    public  void delete(int id){
+        iProductoCrudRepository.deleteById(id);
     }
 
 }
--- src/main/java/co/com/practica/spring/boot/persistence/crud/IProductoCrudRepository.java
+++ src/main/java/co/com/practica/spring/boot/persistence/crud/IProductoCrudRepository.java
@@ -3,4 +3,10 @@ package co.com.practica.spring.boot.persistence.crud;
 import co.com.practica.spring.boot.persistence.entity.Producto;
 import org.springframework.data.repository.CrudRepository;
 
-public interface IProductoCrudRepository extends CrudRepository<Producto,Integer> { }
+import java.util.List;
+import java.util.Optional;
+
+public interface IProductoCrudRepository extends CrudRepository<Producto,Integer> {
+    List<Producto> findByIdCategoriaOrederByNombreAsc(int idCategoria);
+    Optional<List<Producto>> findByCantidadStockLessThanAndEstado(int cantidadStock, boolean estado);
+}
